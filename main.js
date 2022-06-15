d1cb034e4f49e078bb0bbe797fbdd4c5e8cea15c
Christopher Babb <chris@appsoftdev.com>
2022-06-14 18:30:07 -0700

    commit tree acfdb7d4a632d183ea93c694694a1ff02fe60ad7

--- main.js
+++ main.js
@@ -3,7 +3,29 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+  //Hide the Error
+  const errorMessage = document.getElementById('modal')
+  errorMessage.setAttribute('class', 'hidden')
 
+  //Event Listener for Heart
+  const hearts = document.getElementsByClassName('like-glyph')
+  for(let heart of hearts){
+    heart.addEventListener('click', () => mimicServerCall()
+    .then(function(rtn){
+      if(heart.textContent === EMPTY_HEART){
+        heart.textContent = FULL_HEART
+        heart.setAttribute('class', 'activated-heart')
+      }
+      else{
+        heart.textContent = EMPTY_HEART
+        heart.removeAttribute('class', 'activated-heart')
+      }
+    })
+    .catch((e) => {
+      errorMessage.removeAttribute('class', 'hidden') 
+    }))
+  }
+  
 
 
 
@@ -15,6 +37,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
+      console.log(isRandomFailure)
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
